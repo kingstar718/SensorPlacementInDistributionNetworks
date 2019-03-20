6e8b2bb50103b044bbe60c2cadc86f855f86b265
kingstar718 <936016045@qq.com>
2019-03-20 17:18:57 +0800

    generate time matrix code

--- timematrix/generatetimematrix.py
+++ timematrix/generatetimematrix.py
@@ -24,7 +24,7 @@ def generate_timematrix(exepath, inpath, rptpath, sourcequality, duration, qual_
         nodenum = nodenum + 1
         command_line(exepath, inpath, rptpath, nodenum, sourcequality, duration, qual_reportstep)
         waterqualitylist = split_file(rptpath, qualindex, regularmatch)
-        timelist = generate_timelist(waterqualitylist, nodenum, nodeCount, duration, qualindex)
+        timelist = generate_timelist(waterqualitylist, nodenum, nodeCount, duration, qual_reportstep)
         timematrix.append(timelist)
         print("第 " ,nodenum, " 次生成时间矩阵")
     timematrix = np.array(timematrix)
@@ -43,5 +43,17 @@ if __name__ == "__main__":
     qualindex1 = 4
     regularmatch1 = '  [0-9]'
     nodeCount1 = 36
-    net2_matrix = generate_timematrix(exe1,input1,rpt1,sourcequality1,duration1,qual_reportstep1,qualindex1,regularmatch1,nodeCount1)
-    print(net2_matrix)
+    #net2_matrix1 = generate_timematrix(exe1,input1,rpt1,sourcequality1,duration1,qual_reportstep1,qualindex1,regularmatch1,nodeCount1)
+    #print(net2_matrix1)
+
+    exe2 = "D:/迅雷下载/EPANETDEMO.exe"
+    input2 = "D:/迅雷下载/ky2.inp"
+    rpt2 = "D:/迅雷下载/ky2.rpt"
+    sourcequality2 = 200000  # 投入的污染物mg数
+    duration2 = 10800  # 水力模拟时间参数
+    qual_reportstep2 = 600  # 水质步长与报告间隔时间
+    qualindex2 = 4
+    regularmatch2 = '  J'
+    nodeCount2 = 809
+    net2_matrix2 = generate_timematrix(exe2, input2, rpt2, sourcequality2, duration2, qual_reportstep2, qualindex2, regularmatch2, nodeCount2)
+    print(net2_matrix2)
