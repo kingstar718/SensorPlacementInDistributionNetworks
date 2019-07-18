8fd929ee44644e3353a68e2f43f97432be26fa44
kingstar718 <936016045@qq.com>
2019-07-18 21:52:05 +0800

    change

--- simulation/EventDerivation.py
+++ simulation/EventDerivation.py
@@ -5,6 +5,10 @@ from os import path, remove
 from time import time
 from WaterQualitySim import WaterQualitySim
 import networkx
+import numpy as np
+import pandas as pd
+import json
+
 
 class EventDerivation(WaterQualitySim):
     def __init__(self, inp, is_simple=False):
@@ -20,48 +24,63 @@ class EventDerivation(WaterQualitySim):
         end_time = 16*3600
         return super().water_quality(node_name, start_time, end_time, quality, rpt_file_path)
 
-    def compute_derivation(self, node_name):
+    def compute_derivation(self, node_name, start_time, duration):
+        """
+        从主污染事件衍生次级事件
+        :param node_name: 主事件节点名称
+        :param start_time:  起始时间
+        :param duration: 持续时间
+        :return:
+        """
         water_quality = self.water_quality(node_name)
-        node_list = list(water_quality.columns)
-        index_list = list(water_quality.index)
-        node_dirt = {}
+        node_list = list(water_quality.columns)     # 其他节点
+        index_list = list(water_quality.index)          # index为时间
+        node_dirt = {}      # 当前节点污染事件影响的节点字典   {节点：初次污染的时间}
 
-        m_list =[]
-        M_list =[]
+        m_list =[]      # 8小时时污染的节点集合
+        M_list =[]      # 16小时污染的节点集合
         for i in node_list:
             time_list = []
             for n, j in enumerate(water_quality[i]):
                 if j !=0:
                    time_list.append(index_list[n])
-            if len(time_list)>0:
+            if len(time_list)>0 and time_list[0] <=start_time+duration:
                 M_list.append(i)
                 node_dirt[i] = time_list[0]
-            if len(time_list)>0 and time_list[0] <= 28800:
+            if len(time_list)>0 and time_list[0] <= start_time:
                 m_list.append(i)
-        print(len(m_list), len(M_list))
-        print(m_list)
-        print(M_list)
-        print(self.init_model().junction_name_list)
 
+        #print(len(m_list), len(M_list))
+        #print(m_list)
+        #print(M_list)
+        #print(self.init_model().junction_name_list)
+        result_list = []
         G = self.init_model().get_graph()
         G = G.to_directed()
-        import wntr
-        # wntr.graphics.plot_network(self.init_model(), node_attribute=M_list)
         for i in m_list:
             i_list = []
             for j in M_list:
-                b = networkx.has_path(G, i, j)
-                i_list.append(b)
-            print("检查 %s 的连通性："%i ,i_list)
-        print(node_dirt)
-        for i in m_list:
-            print(node_dirt[i])
-            # print(time_list)
+                b = networkx.has_path(G, i, j)      # 判断节点之间是否有路径    判断1
+                if b is True:
+                    i_list.append(j)    # 认为节点i发生的污染时间j能被波及
+            i_dirt = {}
 
-        return water_quality
+            for n in i_list:
+                if node_dirt[i] < node_dirt[n]:
+                    # 判断次级节点污染事件影响的节点在主事件的受到污染的时间是否要早  判断2
+                    # 如果早了，则不是次级事件能影响到的节点
+                    diff_time = node_dirt[n] - node_dirt[i]
+                    if diff_time <= duration:
+                        i_dirt[n] = diff_time
+            i_dirt[i] = 600     # 将当前节点加入
+            # print("当前节点是 %s " % i , node_dirt[i], i_dirt)
+            result_list.append(i_dirt)
+        result_list = np.array(result_list)
+        print(result_list)
+        return result_list
 
 
 if __name__ == "__main__":
     inp2 = "F:/AWorkSpace/Python-Learning-Data/ky2.inp"
     ed = EventDerivation(inp2)
-    quality_data = ed.compute_derivation("J-135")
+    result_list = quality_data = ed.compute_derivation("J-135", 28800, 28800)
